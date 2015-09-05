17172f973a6ca39149e6e31a3c1cfc14b6b59a8a
paulgovan <pgovan1@aggienetwork.com>
2015-09-05 14:36:04 -0500

    Documentation

--- R/sigma.R
+++ R/sigma.R
@@ -1,10 +1,12 @@
 #' An htmlwidget wrapper for the sigmajs graph visualization library.
 #' 
 #' @param gexf GEXF (Graph Exchange XML Format) data file.
-#' @param drawEdges logical, whether or not to draw the edges
-#' @param drawNodes logical, whether or not to draw the nodes
-#' @param width the width of the graph in pixels
-#' @param height the height of the graph in pixels
+#' @param drawEdges logical, whether or not to draw the edges.
+#' @param drawNodes logical, whether or not to draw the nodes.
+#' @param node.type character string indicating the node shape. Options include
+#' \code{circle}, \code{square}, \code{diamond}, and \code{equilateral}.
+#' @param width the width of the graph in pixels.
+#' @param height the height of the graph in pixels.
 #' @import htmlwidgets
 #' 
 #' @source sigmajs was developed by  ALEXIS JACOMY with the help of  GUILLAUME PLIQUE. See \url{http://sigmajs.org/} 
@@ -12,7 +14,7 @@
 #' data <- system.file("examples/ediaspora.gexf.xml", package = "sigma")
 #' sigma(data)
 #' @export
-sigma <- function(gexf, drawEdges = TRUE, drawNodes = TRUE,
+sigma <- function(gexf, drawEdges = TRUE, drawNodes = TRUE, node.type = "circle",
                   width = NULL, height = NULL) {
   
   # read the gexf file
@@ -21,7 +23,8 @@ sigma <- function(gexf, drawEdges = TRUE, drawNodes = TRUE,
   # create a list that contains the settings
   settings <- list(
     drawEdges = drawEdges,
-    drawNodes = drawNodes
+    drawNodes = drawNodes,
+    node.type = node.type
   )
   
   # pass the data and settings using 'x'
@@ -34,11 +37,27 @@ sigma <- function(gexf, drawEdges = TRUE, drawNodes = TRUE,
   htmlwidgets::createWidget("sigma", x, width = width, height = height)
 }
 
+#' A Shiny output function for the sigmajs graph visualization library.
+#' 
+#' @param outputId the output variable to read from.
+#' @param width the width of the graph in percentage.
+#' @param height the height of the graph in pixels.
+#' @import htmlwidgets
+#' 
+#' @source sigmajs was developed by ALEXIS JACOMY with the help of  GUILLAUME PLIQUE. See \url{http://sigmajs.org/} 
 #' @export
 sigmaOutput <- function(outputId, width = "100%", height = "400px") {
   shinyWidgetOutput(outputId, "sigma", width, height, package = "sigma")
 }
 
+#' A Shiny render function for the sigmajs graph visualization library.
+#' 
+#' @param expr An expression that generates an HTML widget.
+#' @param env The environment in which to evaluate expr.
+#' @param quoted Is expr a quoted expression (with quote())? This is useful if you want to save an expression in a variable.
+#' @import htmlwidgets
+#' 
+#' @source sigmajs was developed by ALEXIS JACOMY with the help of  GUILLAUME PLIQUE. See \url{http://sigmajs.org/} 
 #' @export
 renderSigma <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
